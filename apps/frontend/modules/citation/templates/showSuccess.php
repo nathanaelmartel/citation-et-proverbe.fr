0393c7efe05f1082cd81a941c0a1b0ee8aa766b4
Nathanaël Martel <nathanael@fam-martel.eu>
2011-06-11 00:30:15 +0200

    adsense

--- apps/frontend/modules/citation/templates/showSuccess.php
+++ apps/frontend/modules/citation/templates/showSuccess.php
@@ -1,2 +1,14 @@
 
       <?php include_partial('citation/citation', array('citation' => $citation)) ?>
+      
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-6736033252489950";
+/* alpha */
+google_ad_slot = "3750193663";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
