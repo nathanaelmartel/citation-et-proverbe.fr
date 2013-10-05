a134f55694c06d5f5e4ec618ddce3d94a6deee4a
Nathanaël <nathanael@fam-martel.eu>
2013-10-05 23:45:40 +0200

    analytics

--- apps/frontend/templates/_piwik.php
+++ apps/frontend/templates/_piwik.php
@@ -1,40 +1,6 @@
-
-
-	<script type="text/javascript">
-	 var _gaq = _gaq || [];
-	 _gaq.push(['_setAccount', 'UA-11137972-2']);
-	 _gaq.push(['_trackPageview']);
-
-	 (function() {
-	   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	   var s = document.getElementsByTagName('script')[0];
-		 s.parentNode.insertBefore(ga, s);
-	 })();
-	</script>
-
-
-	<!-- Piwik -->
-	<script type="text/javascript">
-	var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.fam-martel.eu/" : "http://piwik.fam-martel.eu/");
-	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-	</script><script type="text/javascript">
-	try {
-		var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 5);
-		piwikTracker.disableCookies();
-		piwikTracker.trackPageView();
-		piwikTracker.enableLinkTracking();
-	} catch( err ) {}
-	</script><noscript><p><img src="http://piwik.fam-martel.eu/piwik.php?idsite=5" style="border:0" alt="" /></p></noscript>
-	<!-- End Piwik Tag -->
-
-
-
-
-
 <?php 
 
-/*
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
 {
 	require_once sfConfig::get('sf_lib_dir').'/vendor/piwik/PiwikTracker.php';
@@ -43,4 +9,4 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
 	$piwikTracker = new PiwikTracker( $idSite = 5 );
 	$piwikTracker->doTrackPageView($sf_context->getResponse()->getTitle());
 }
-*/
+
